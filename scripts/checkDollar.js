ac484ea1fbfd620bbeb0070d3f22d3d64649bba1
Aylen Palma <aylenpalmaw@gmail.com>
2026-05-30 16:40:50 -0300

    fix supabase realtime for github actions

--- scripts/checkDollar.js
+++ scripts/checkDollar.js
@@ -27,7 +27,12 @@ SUPABASE
 
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_ANON_KEY
+  process.env.VITE_SUPABASE_ANON_KEY,
+  {
+    realtime: {
+      enabled: false
+    }
+  }
 )
 
 /*
